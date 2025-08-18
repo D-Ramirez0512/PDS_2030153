4bed7b11fc8f90208f95f6b545e39a04a708ae0d
D-Ramirez0512 <diegoramirezjasso@gmail.com>
2025-08-17 20:35:51 -0600

    sobres

--- src/utils/grapher.py
+++ src/utils/grapher.py
@@ -1,27 +1,47 @@
 import matplotlib.pyplot as plt
 
+def signal_graphs(t_continue, t_discrete, x_continue, x_discrete, title):
+    plt.figure(figsize=(8, 8))
+    plt.suptitle(title)
 
-def graficador_continuo(
-        var_indep, var_dep,
-        titulo: str = "", etiqueta: str = "",
-        etiqueta_x: str = "", etiqueta_y: str = ""):
-    plt.plot(var_indep, var_dep, label=etiqueta)
-    plt.title(titulo)
-    plt.xlabel(etiqueta_x)
-    plt.ylabel(etiqueta_y)
-    plt.legend(loc='upper right')
-    plt.grid(True)
+    # Continue signal
+    plt.subplot(3, 1, 1)
+    plt.plot(t_continue, x_continue, 'r')
+    plt.title('Continue signal')
+    plt.xlabel('Time')
+    plt.ylabel('Amplitude')
+    plt.grid()
+
+    # Discrete signal
+    plt.subplot(3, 1, 2)
+    plt.stem(t_discrete, x_discrete, basefmt='k-')
+    plt.title('Discrete signal')
+    plt.xlabel('Time')
+    plt.ylabel('Amplitude')
+    plt.grid()
+
+    # Overlaid signals
+    plt.subplot(3, 1, 3)
+    plt.plot(t_continue, x_continue, 'r', label='Continue')
+    plt.stem(t_discrete, x_discrete, basefmt='k-', label='Discrete')
+    plt.title('Overlapping signals')
+    plt.xlabel('Time')
+    plt.ylabel('Amplitude')
+    plt.legend()
+    plt.grid()
+
+    plt.tight_layout(rect=[0, 0, 1, 0.95])
     plt.show()
 
 
-def graficador_discreto(
-        var_indep, var_dep,
-        titulo: str = "", etiqueta: str = "",
-        etiqueta_x: str = "", etiqueta_y: str = ""):
-    plt.stem(var_indep, var_dep, label=etiqueta)
-    plt.title(titulo)
-    plt.xlabel(etiqueta_x)
-    plt.ylabel(etiqueta_y)
-    plt.legend(loc='upper right')
-    plt.grid(True)
+def dac_graphs(d_in, a_out, voltage, levels, bits):
+    plt.figure(figsize=(8, 6))
+    plt.suptitle(f'DAC {bits} bits')
+    
+    plt.step(d_in, a_out, where='post', color='r')
+    plt.xlabel('Digital input level')
+    plt.ylabel('Analog voltage')
+    plt.grid()
+    plt.ylim(-0.1, voltage + 0.1)
+    plt.xlim(-1, levels)
     plt.show()
